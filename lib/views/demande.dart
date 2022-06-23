a723079d446afd4e83806393fbfd418c358caa57
Nasreddine Bouchemel <kn_bouchemel@esi.dz>
2022-06-23 12:55:10 +0100

    lamghi

---
+++ lib/views/demande.dart
@@ -0,0 +1,56 @@
+import 'package:cnas/config/size_config.dart';
+import 'package:flutter/material.dart';
+
+class DemandesView extends StatefulWidget {
+  const DemandesView({Key? key}) : super(key: key);
+
+  @override
+  State<DemandesView> createState() => _DemandesViewState();
+}
+
+class _DemandesViewState extends State<DemandesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            height: SizeConfig.screenHeight * 0.8,
+            width: SizeConfig.screenWidth * 0.3,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius:
+                  BorderRadius.circular(SizeConfig.screenHeight * 0.05),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: SizeConfig.screenHeight * 0.8,
+            width: SizeConfig.screenWidth * 0.5,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius:
+                  BorderRadius.circular(SizeConfig.screenHeight * 0.05),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,55 @@
+import 'package:cnas/config/const.dart';
+import 'package:cnas/config/size_config.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final List<Widget> _widgets = [
+    Container(
+      child: Text(
+        'home',
+      ),
+    ),
+    Container(child: Text('demande')),
+    Container(child: Text('ETS')),
+  ];
+
+  int selectedWidgetIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      body: Row(
+        children: [
+          NavigationRail(
+            destinations: [
+              NavigationRailDestination(
+                  icon: Icon(Icons.home), label: Text('home')),
+              NavigationRailDestination(
+                  icon: Icon(Icons.request_page), label: Text('demande')),
+              NavigationRailDestination(
+                  icon: Icon(Icons.delivery_dining_rounded),
+                  label: Text('ETS')),
+            ],
+            selectedIndex: selectedWidgetIndex,
+            onDestinationSelected: (index) {
+              setState(() {
+                selectedWidgetIndex = index;
+              });
+            },
+          ),
+          Expanded(
+            child: _widgets[selectedWidgetIndex],
+          ),
+        ],
+      ),
+    );
+  }
+}
