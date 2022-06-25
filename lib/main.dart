a98c4e45ea1b1b26db6f8d1d8b0d7a7d2643ff42
Nasreddine Bouchemel <kn_bouchemel@esi.dz>
2022-06-25 16:39:07 +0100

    side menu

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,29 @@ import 'views/auth/loginView.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (context) => PatientViewModel()),
-    ChangeNotifierProvider(create: (context) => LoginViewModel()),
-  ], child: const MyHomePage()));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => PatientViewModel()),
+        ChangeNotifierProvider(create: (context) => LoginViewModel()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -4,6 +4,7 @@ import 'package:cnas/views/demande.dart';
 import 'package:cnas/views/auth/loginView.dart';
 import 'package:cnas/views/demande/list_demande_view.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,41 +17,72 @@ class _HomeState extends State<Home> {
   final List<Widget> _widgets = [
     ListDemande(),
     Container(child: Text('transports')),
-    Container(child: Text('reclamation')),
     Container(child: Text('profile')),
+    Container(child: Text('reclamation')),
   ];
+
+  PageController pageController = PageController();
 
   int selectedWidgetIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    List<SideMenuItem> items = [
+      SideMenuItem(
+        priority: 0,
+        title: 'Demandes',
+        onTap: () => pageController.jumpToPage(0),
+        icon: Icon(
+          Icons.add_box,
+          color: lightRed,
+        ),
+      ),
+      SideMenuItem(
+        priority: 1,
+        title: 'Transports',
+        onTap: () => pageController.jumpToPage(1),
+        icon: Icon(Icons.local_shipping),
+      ),
+      SideMenuItem(
+        priority: 2,
+        title: 'Profile',
+        onTap: () => pageController.jumpToPage(2),
+        icon: Icon(Icons.person),
+      ),
+      SideMenuItem(
+        priority: 3,
+        title: 'Reclamation',
+        onTap: () => pageController.jumpToPage(3),
+        icon: Icon(Icons.description),
+      ),
+    ];
+
     SizeConfig().init(context);
     return Scaffold(
       backgroundColor: Color(0xFFF6F6F6),
       body: Row(
         children: [
-          NavigationRail(
-            destinations: [
-              NavigationRailDestination(
-                  icon: Icon(Icons.home), label: Text('demande')),
-              NavigationRailDestination(
-                  icon: Icon(Icons.request_page), label: Text('transports')),
-              NavigationRailDestination(
-                  icon: Icon(Icons.delivery_dining_rounded),
-                  label: Text('reclamation')),
-              NavigationRailDestination(
-                  icon: Icon(Icons.delivery_dining_rounded),
-                  label: Text('profile')),
-            ],
-            selectedIndex: selectedWidgetIndex,
-            onDestinationSelected: (index) {
-              setState(() {
-                selectedWidgetIndex = index;
-              });
-            },
+          SideMenu(
+            items: items,
+            controller: pageController,
+            style: SideMenuStyle(
+              selectedIconColor: red,
+            ),
           ),
           Expanded(
-            child: _widgets[selectedWidgetIndex],
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: SizeConfig.screenHeight * 0.03,
+                  left: SizeConfig.screenWidth * 0.05,
+                  right: SizeConfig.screenWidth * 0.05,
+                ),
+                child: PageView(
+                  controller: pageController,
+                  children: _widgets,
+                ),
+              ),
+            ),
           ),
         ],
       ),
